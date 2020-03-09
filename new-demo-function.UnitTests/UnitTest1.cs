b604fd479b56c3af8845e2331a8f8b7f8092f6eb
Himanshu Trivedi <himanshu.trivedi@hotmail.com>
2020-03-09 14:36:37 +0000

    New function app

---
+++ new-demo-function.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace new_demo_function.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
